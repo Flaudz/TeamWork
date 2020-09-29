5d1eb47767e7b4d0fda984da4ca54d6b983152bb
Flaudz <nico936d@edu.campusvejle.dk>
2020-09-29 11:58:45 +0200

    Made a search function and made games list in gamepage

--- js/GamePage.js
+++ js/GamePage.js
@@ -2,4 +2,26 @@ slideShow(".newGamesSlideShow", 5);
 
 let gamesArray = [];
 
-for (let i = 0; i < document.querySelector(".games").childElementCount; i++) {}
+for (let i = 0; i < document.querySelector(".games").childElementCount; i++) {
+	gamesArray.push(
+		document.querySelector(".games").children[i].className.toLowerCase()
+	);
+	console.log(gamesArray);
+}
+
+const gamesSearchInput = document.querySelector("#searchGameInput");
+
+gamesSearchInput.addEventListener("search", () => {
+	gamesArray.forEach((game) => {
+		let gameDiv = document.getElementsByClassName(`${game}`);
+		if (
+			game.includes(gamesSearchInput.value.toLowerCase()) ||
+			gamesSearchInput.value.toLowerCase() == game
+		) {
+			gameDiv[0].style.display = "block";
+		} else {
+			console.log("asd");
+			gameDiv[0].style.display = "none";
+		}
+	});
+});
