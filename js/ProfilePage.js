7e6248d8de1c7e5c1b7b4d79ed37c13fd4e64f7c
Flaudz <nico936d@edu.campusvejle.dk>
2020-10-01 14:10:16 +0200

    Made navigation go in and out by class

--- js/ProfilePage.js
+++ js/ProfilePage.js
@@ -9,3 +9,30 @@ fetch("https://json.geoiplookup.io/")
 				flagImg.src = `https://flagcdn.com/224x168/${myresponse.country.ToLower()}.png`;
 			});
 	});
+
+const navigation = () => {
+	const navigation = document.querySelector(".hideNavigation");
+	const size = navigation.clientWidth;
+	navigation.style.transition = "transform 0.4s ease-in-out";
+	navigation.style.transform = `translateX(${-size}px)`;
+	const mainsection = document.querySelector("#mainSection");
+	mainsection.style.transition = "transform 0.8s ease-in-out";
+	mainsection.classList.add("mainSection");
+	mainsection.setAttribute("id", "");
+};
+
+navigation();
+
+const closeNavigationOnResize = () => {
+	const navigation = document.querySelector("#navigation");
+	if (navigation.classList.contains("hideNavigation")) {
+		const navigation = document.querySelector(".hideNavigation");
+		const size = navigation.clientWidth;
+		navigation.style.transition = "transform 0.0s ease-in-out";
+		navigation.style.transform = `translateX(${-size}px)`;
+	}
+};
+
+window.addEventListener("resize", () => {
+	closeNavigationOnResize();
+});
